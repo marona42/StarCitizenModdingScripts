92fd96fb04d97ea74ab89b3c4748de9bb26ecc5b
marona42 <marona42@naver.com>
2021-01-31 11:20:39 +0900

    add export_target

--- export-smartcat.py
+++ export-smartcat.py
@@ -100,6 +100,50 @@ def export_multilang_csv(document_resource, document_id, output_file_name):
         print('Export completed')
         return 0
 
+def export_target(document_resource, document_id, output_file_name):
+    print(f'Export document: {output_file_name}')
+    with document_resource.request_export(document_ids=document_id, target_type='target') as export_result:
+        if export_result.status_code != 200:
+            print('Error: Failed request status code - ', export_result.status_code)
+            return 1
+        print('Export started')
+        export_json = json.loads(export_result.text)
+        print("export info:")
+        print(export_json)
+        export_task_id = export_json['id']
+        wait_download_ready = False
+        try:
+            while True:
+                with document_resource.download_export_result(export_task_id) as download_result:
+                    if download_result.status_code == 200:
+                        if wait_download_ready:
+                            print('')
+                        wait_download_ready = False
+                        print('Download started')
+                        with open(output_file_name, 'wb') as out_file:
+                            for chunk in download_result.iter_content(chunk_size=8192):
+                                out_file.write(chunk)
+                        print('Download completed')
+                        break
+                    elif download_result.status_code == 204:
+                        if wait_download_ready:
+                            print('.', end='')
+                        else:
+                            print('Wait download ready', end='')
+                            wait_download_ready = True
+                        time.sleep(1)
+                    else:
+                        if wait_download_ready:
+                            print('')
+                        print('Error: export status code - ', download_result.status_code)
+                        return 1
+        except:
+            if wait_download_ready:
+                print('')
+            raise
+        print('Export completed')
+        return 0
+
 def main(args):
     result = 1
     config_name = ''
